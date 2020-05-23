49056f920b7f8a2e4f8acb0fca5ba17663656a42
malugrave <malugrave@nacad.ufrj.br>
2020-05-23 10:21:00 -0300

    Update

--- Monitoramento/covid_world_abs.m
+++ Monitoramento/covid_world_abs.m
@@ -57,17 +57,17 @@ if (plot_type == 1)
 if (init == 6) country = 'Brazil'; color = [0,0,0]/255; linew = 1.75; end
 if (init == 15) country = 'South Korea'; color = [69,169,0]/255; end
 if (init == 10) country = 'Turkey'; color = [96,209,224]/255; end  
-if (init == 12) country = 'Peru'; color = [181,147,87]/255; end
+if (init == 13) country = 'Peru'; color = [181,147,87]/255; end
 if (init == 9) country = 'Iran'; color = [255,130,113]/255; end
 if (init == 8) country = 'Germany'; color = [209,227,105]/255; end
 if (init == 14) country = 'Chile'; color = [248,187,208]/255; end
 if (init == 1) country = 'United States'; color = [0,104,44]/255; end
-if (init == 4) country = 'France'; color = [0,45,135]/255; end
+if (init == 5) country = 'France'; color = [0,45,135]/255; end
 if (init == 2) country = 'United Kingdom'; color = [135,85,30]/255; end
 if (init == 3) country = 'Italy'; color = [203,63,23]/255; end
-if (init == 5) country = 'Spain'; color = [191,171,72]/255; end
+if (init == 4) country = 'Spain'; color = [191,171,72]/255; end
 if (init == 7) country = 'Belgium'; color = [236,64,122]/255; end
-if (init == 13) country = 'Russia'; color = [0.4,0.4,0.4]; end
+if (init == 12) country = 'Russia'; color = [0.4,0.4,0.4]; end
 if (init == 11) country = 'Sweden'; color = [0,0.5,0.6]; end
 
 location = data(find(strcmp([all_data.location], country)),1:8);
@@ -95,7 +95,7 @@ end
 
 if (plot_type == 2)
 %ordem por países que tem mais caso
-if (init == 3) country = 'Brazil'; color = [0,0,0]/255; linew = 1.75; end
+if (init == 2) country = 'Brazil'; color = [0,0,0]/255; linew = 1.75; end
 if (init == 15) country = 'South Korea'; color = [69,169,0]/255; end
 if (init == 8) country = 'Turkey'; color = [96,209,224]/255; end  
 if (init == 11) country = 'Peru'; color = [181,147,87]/255; end
@@ -108,7 +108,7 @@ if (init == 4) country = 'United Kingdom'; color = [135,85,30]/255; end
 if (init == 6) country = 'Italy'; color = [203,63,23]/255; end
 if (init == 5) country = 'Spain'; color = [191,171,72]/255; end
 if (init == 13) country = 'Belgium'; color = [236,64,122]/255; end
-if (init == 2) country = 'Russia'; color = [0.4,0.4,0.4]; end
+if (init == 3) country = 'Russia'; color = [0.4,0.4,0.4]; end
 if (init == 14) country = 'Sweden'; color = [0,0.5,0.6]; end
 
 
