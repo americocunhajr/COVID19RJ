c318bcf07f3c68a977feb43f4a1800eb7b286e03
malugrave <malugrave@nacad.ufrj.br>
2020-06-03 10:18:15 -0300

    Update

--- Monitoramento/script_WD.m
+++ Monitoramento/script_WD.m
@@ -1,15 +1,15 @@
 clear all;
 close all;
 clc;
-
-%covid19rj_M_ST_WD_VA
-%covid19rj_M_ST_WD_PM
-%covid19rj_M_MC_WD
-%covid19rj_M_CP_WD
-
-
-
-%covid19rj_M_ST_AL_VA
-%covid19rj_M_ST_AL_PM
+% 
+% covid19rj_M_ST_WD_VA
+% covid19rj_M_ST_WD_PM
+% covid19rj_M_MC_WD
+% covid19rj_M_CP_WD
+% 
+% 
+% 
+% covid19rj_M_ST_AL_VA
+covid19rj_M_ST_AL_PM
 covid19rj_M_MC_AL
 covid19rj_M_CP_AL
